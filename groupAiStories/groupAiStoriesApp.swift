b17d496ae8161f3a1d8ec84c2609eeca7c5b19bb
PrimeMinus <96957546+PrimeMinus@users.noreply.github.com>
2025-05-11 13:24:54 -0400

    Initial Commit

---
+++ groupAiStories/groupAiStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  groupAiStoriesApp.swift
+//  groupAiStories
+//
+//  Created by Brian Warner on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct groupAiStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ groupAiStoriesTests/groupAiStoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  groupAiStoriesTests.swift
+//  groupAiStoriesTests
+//
+//  Created by Brian Warner on 5/11/25.
+//
+
+import Testing
+@testable import groupAiStories
+
+struct groupAiStoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
